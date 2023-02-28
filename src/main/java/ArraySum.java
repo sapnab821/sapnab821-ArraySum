2e369c5e0cb4197499ebd647900ec9d08cdca214
sapnab821 <sapnabolikal@gmail.com>
2023-02-28 21:07:54 +0000

    Array Sum

--- src/main/java/ArraySum.java
+++ src/main/java/ArraySum.java
@@ -7,7 +7,11 @@ public class ArraySum {
      * @return the sum of all values in arr.
      */
     public int sum(int[] arr){
-
-        return 0;
+        int finSum=0;
+        for(int i=0; i<arr.length; i++){
+            finSum=finSum+arr[i];
+        }
+        return finSum;
+    
     }
 }
